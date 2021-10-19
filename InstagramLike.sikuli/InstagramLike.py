65f949174c3972c6eee79aef328cd0c014916714
Роман <quoe@mail.ru>
2021-10-19 23:40:55 +0300

    Чтобы не всплывали окна по ссылке

--- InstagramLike.sikuli/InstagramLike.py
+++ InstagramLike.sikuli/InstagramLike.py
@@ -1,15 +1,18 @@
 import random
-Settings.MoveMouseDelay = 0.02
-iter = 200
+Settings.MoveMouseDelay = 0.01
+iter = 2
 count = random.randrange(iter, iter * 2)
 i = 1
 while i <= count:
     try:
         if exists(Pattern("1609497574296.png").similar(0.95)):
             click(Pattern("1609497574296.png").similar(0.95))
+            if exists(Pattern("1634675207891.png").similar(0.95)):
+                hover(Pattern("1634675207891.png").similar(0.95).targetOffset(-50,0)) #Чтобы не всплывали окна по ссылке профиля
         type(Key.PAGE_DOWN)
     except:
         type(Key.PAGE_DOWN)
+    print(count-i)
     i += 1
-
+    
 
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Бот на Sikuli лайкает фотки в Инсте  
 Код можно посмотреть в InstagramLike.sikuli/InstagramLike.html.  
 Для открытия проекта в Sikuli надо открыть папку InstagramLike.sikuli.  
+
+Для запуска из cmd по инструкции
+https://github.com/RaiMan/SikuliX1/wiki/How-to-make-Jython-ready-in-the-IDE
+Скачать
+Jython standalone jar package 2.7
+И поместить в папку с "Sikuli\sikulixide-2.0.4.jar" и запускать через cmd вида:
+java -jar "D:\Sikuli\sikulixide-2.0.4.jar" -r "D:\Sikuli_App\InstagramLike.sikuli"
